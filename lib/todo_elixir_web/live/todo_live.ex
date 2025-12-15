d0317d3dcf98cc44d1a1d503160ca965e8c82ab6
kau4xz <kauagaleno09@gmail.com>
2025-12-14 22:36:53 -0300

    adicionando função de concluir task

--- lib/todo_elixir_web/live/todo_live.ex
+++ lib/todo_elixir_web/live/todo_live.ex
@@ -30,6 +30,14 @@ defmodule TodoElixirWeb.TodoLive do
     {:noreply, assign(socket, tasks: Tasks.list_tasks())}
   end
 
+  def handle_event("toggle_complete", %{"id" => id}, socket) do
+    id = String.to_integer(id)
+    task = Tasks.get_task!(id)
+    {:ok, _} = Tasks.toggle_complete(task)
+
+    {:noreply, assign(socket, tasks: Tasks.list_tasks())}
+  end
+
   def render(assigns) do
     ~H"""
     <main style="max-width: 600px; margin: 40px auto; font-family: sans-serif;">
@@ -43,7 +51,17 @@ defmodule TodoElixirWeb.TodoLive do
       <ul>
         <%= for task <- @tasks do %>
           <li style="display:flex; gap:10px; align-items:center; margin-top:8px;">
-            <span><%= task.title %></span>
+            <span style={if task.done, do: "text-decoration: line-through; opacity: 0.7;", else: ""}>
+              <%= task.title %>
+            </span>
+
+            <button
+              type="button"
+              phx-click="toggle_complete"
+              phx-value-id={task.id}
+            >
+              <%= if task.done, do: "Desmarcar", else: "Concluir" %>
+            </button>
 
             <button
               type="button"
